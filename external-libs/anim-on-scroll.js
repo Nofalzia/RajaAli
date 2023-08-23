b0f446bf693b20adc800a8da8a796d01bc88d3d2
Nofal Zia <nofalzia4S@gmail.com>
2023-08-23 14:54:37 +0500

    First Commit

---
+++ external-libs/anim-on-scroll.js
@@ -0,0 +1,17 @@
+      //Animation on Scroll
+
+  const observer = new IntersectionObserver( (enteries) => {
+    enteries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('animate__animated');
+            entry.target.classList.add('show');
+        }
+
+    })
+});
+
+
+
+const hiddenelements = document.querySelectorAll('.hidden')
+  hiddenelements.forEach((el)=> observer.observe(el));
---
+++ js/gallery-anim.js
@@ -0,0 +1,65 @@
+const track = document.getElementById("image-track");
+const heading = document.getElementById("Heading");
+
+var trackRect = track.getBoundingClientRect();
+var headingRect = heading.getBoundingClientRect();
+
+if(trackRect.bottom > headingRect.top 
+  && trackRect.right > headingRect.left 
+  && trackRect.top < headingRect.bottom 
+  && trackRect.left < headingRect.right) {
+      heading.classList.add("selected");
+      console.log("nofal is gay")
+  }
+
+const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+};
+
+const handleOnMove = (e) => {
+  if (track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentageUnconstrained =
+      parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+  track.dataset.percentage = nextPercentage;
+
+  track.animate(
+    {
+      transform: `translate(${nextPercentage}%, -50%)`,
+    },
+    { duration: 1200, fill: "forwards" }
+  );
+
+  for (const image of track.getElementsByClassName("image")) {
+    image.animate(
+      {
+        objectPosition: `${100 + nextPercentage}% center`,
+      },
+      { duration: 1200, fill: "forwards" }
+    );
+  }
+};
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = (e) => handleOnDown(e);
+
+window.ontouchstart = (e) => handleOnDown(e.touches[0]);
+
+window.onmouseup = (e) => handleOnUp(e);
+
+window.ontouchend = (e) => handleOnUp(e.touches[0]);
+
+window.onmousemove = (e) => handleOnMove(e);
+
+window.ontouchmove = (e) => handleOnMove(e.touches[0]);
+
