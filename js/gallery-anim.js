f5a5f64217b80a98f1a90869cd3d43328dd66b6f
Nofal Zia <nofalzia4S@gmail.com>
2023-08-29 22:29:35 +0500

    New Scroll Slider and More Updates

--- js/gallery-anim.js
+++ js/gallery-anim.js
@@ -22,33 +22,21 @@ videos.forEach((video) => {
   observer.observe(video);
 });
 
-// Smooth scrolling behavior
-document.addEventListener("DOMContentLoaded", function () {
-  const navLinks = document.querySelectorAll("nav ul li a");
-  navLinks.forEach(link => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      const targetId = this.getAttribute("href");
-      const targetSection = document.querySelector(targetId);
-      if (targetSection) {
-        targetSection.scrollIntoView({ behavior: "smooth" });
+const lazyVideos = document.querySelectorAll('video[data-src]');
+
+const lazyLoad = target => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const video = entry.target;
+        video.src = video.getAttribute('data-src');
+        video.removeAttribute('data-src');
+        observer.disconnect();
       }
     });
   });
-});
 
-// Video Carousel (Auto-slide)
-const carouselTrack = document.querySelector('.carousel-track');
-const slides = Array.from(document.querySelectorAll('.carousel-slide'));
-let currentIndex = 0;
+  io.observe(target);
+};
 
-function updateCarousel() {
-  carouselTrack.style.transform = `translateX(-${currentIndex * 33.33}%)`;
-}
-
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  updateCarousel();
-}
-
-setInterval(nextSlide, 5000); // Auto-slide every 5 seconds
+lazyVideos.forEach(lazyLoad);
