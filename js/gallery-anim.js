1a6e7b52748664f6200d2b52c0b3cb1ed67532a9
Nofal Zia <nofalzia4S@gmail.com>
2023-09-07 20:54:02 +0500

    Bug fixes and Major Improvments in Video Galleries

--- js/gallery-anim.js
+++ js/gallery-anim.js
@@ -35,3 +35,41 @@ document.addEventListener('DOMContentLoaded', function () {
       });
       
     });
+
+    document.addEventListener('DOMContentLoaded', function () {
+      const videoThumbnails = document.querySelectorAll('.vids');
+      const videoPlayer = document.getElementById('video-player-new'); // Unique ID for the new video player
+      const videoPopup = document.querySelector('.video-popup-new'); // Unique class for the new video popup
+      const closePopupButton = document.getElementById('close-popup-new'); // Unique ID for the close button in the new gallery
+    
+      // Hide the new video player initially
+      videoPlayer.style.display = 'none';
+    
+      videoThumbnails.forEach((thumbnail) => {
+        thumbnail.addEventListener('click', () => {
+          const mediaSrc = thumbnail.querySelector('video').getAttribute('data-src');
+          if (mediaSrc) {
+            // Set the new video source
+            videoPlayer.src = mediaSrc;
+            videoPlayer.style.display = 'block';
+    
+            // Programmatically play the new video
+            videoPlayer.play().catch((error) => {
+              // Autoplay was prevented, you can handle this here
+              console.error('Autoplay prevented:', error);
+            });
+    
+            // Show the new video popup
+            videoPopup.style.display = 'flex';
+          }
+        });
+      });
+    
+      closePopupButton.addEventListener('click', () => {
+        // Hide the new video popup, pause the new video, and reset the source
+        videoPopup.style.display = 'none';
+        videoPlayer.pause();
+        videoPlayer.src = '';
+      });
+    });
+    
